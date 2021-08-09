aee47ef26342fb7173ed055f2af6253554ccd521
Manas Mishra <mmanas@vmware.com>
2021-08-09 18:17:12 +0530

    Learned custom writable store

---
+++ 004.trying_custom_stores/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import {count} from './cutom_stores'
+</script>
+
+<h1>Count : {$count}</h1>
+<button on:click={count.increment}>INCREMENT</button>
+<button on:click={count.decrement}>DECREMENT</button>
+<button on:click={count.reset}>RESET</button>
