b6c6ff9f2dd5257d536f1a81d43adb9a4496746e
Manas Mishra <mmanas@vmware.com>
2021-08-09 18:27:29 +0530

    Learned readable stores

---
+++ 005.trying_readable_store/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import {time} from './readable_store'
+	const formatter = Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: "numeric",
+		minute: "2-digit",
+		second: "2-digit"
+	})
+</script>
+
+<h1>{formatter.format($time)}</h1>
