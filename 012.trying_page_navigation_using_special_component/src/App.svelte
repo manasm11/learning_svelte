993013e43b22c2da6cbfad58d73e12a75c4ec228
Manas Mishra <mmanas@vmware.com>
2021-08-16 18:38:57 +0530

    Conclusion: Couldn't come up with any practical situation of using component without complicating things even more

--- 012.trying_page_navigation_using_special_component/src/App.svelte
+++ 012.trying_page_navigation_using_special_component/src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+	import Login from "./Login.svelte";
+	import Feed from './Feed.svelte'
+
+	const cmps = {
+		login: {
+			comp: Login,
+			props: {
+				"on:click": ()=>console.log("LOGGED IN")
+			}
+		},
+		feed: {
+
+		}
+	};
+
+	let name = "login";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<svelte:component this={cmps[name].comp} {...cmps[name].props} />
---
+++ 012.trying_page_navigation_using_special_component/src/Feed.svelte
@@ -0,0 +1,13 @@
+<script>
+    let posts = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E"
+    ]
+</script>
+
+{#each posts as post}
+    <div>{post}</div>
+{/each}
