a473bc3980fc14ae80c5589af05766a8082d1d8a
Manas Mishra <mmanas@vmware.com>
2021-08-16 19:21:08 +0530

    Added way to store and show directory structure

---
+++ 014.learning_self_component/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Node from "./Node.svelte";
+
+	let directory_structure = {
+		root: [
+			{ dir1: [{ subdir11: [{ subsubdir1: [] }, { subdir12: [] }] }] },
+			{ dir2: [{ subdir21: [] }] },
+		],
+	};
+</script>
+
+<h1>HELLO</h1>
+{#each directory_structure.root as member}
+	<Node {member} />
+{/each}
---
+++ 014.learning_self_component/src/Node.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let member;
+
+    let name = Object.keys(member)[0];
+</script>
+
+<h1>{name}</h1>
+<ul>
+    {#each member[name] as m}
+        <li><svelte:self member={m} /></li>
+    {/each}
+</ul>
+
+<style>
+    li {
+        margin-left: 2rem;
+    }
+</style>
