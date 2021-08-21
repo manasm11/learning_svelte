49dd8f89eb7c814042fc3c04963563cab3604116
Manas Mishra <mmanas@vmware.com>
2021-08-21 13:07:18 +0530

    Created a descent algo to show required fields

---
+++ 016.learning_form_validation/src/components/FormElement.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let name;
+    export let type = "text";
+    export let required = false;
+    let _required = false;
+    let _touched = false;
+
+    $:_required = required && _touched
+
+</script>
+
+<label>
+    <b>{name}</b>
+    <input
+        {type}
+        on:blur={() => (_touched = true)}
+        required={_required}
+    />
+</label>
+
+<style>
+    :invalid {
+        background-color: rgb(255, 234, 234);
+        border: red solid 1px;
+    }
+</style>
---
+++ 016.learning_form_validation/src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import FormElement from "../components/FormElement.svelte";
+</script>
+
+<main>
+	<form>
+		<FormElement name="First Name" required/>
+		<FormElement name="Last Name"/>
+		<br>
+		<FormElement name="Email" type="email"/>
+	</form>
+</main>
