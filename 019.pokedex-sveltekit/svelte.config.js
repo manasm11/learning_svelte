12cefb869bbc687b552f20aff4695ffbe54f7a62
Manas Mishra <mmanas@vmware.com>
2021-08-27 11:22:42 +0530

    Added vercel configurations

--- 019.pokedex-sveltekit/svelte.config.js
+++ 019.pokedex-sveltekit/svelte.config.js
@@ -1,11 +1,11 @@
 import preprocess from 'svelte-preprocess';
 import path from 'path';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
@@ -20,12 +20,13 @@ const config = {
 				}
 			}
 		},
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		// adapter: adapter({
+		// 	// default options are shown
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: null
+		// }),
+		adapter: vercel()
 	}
 };
 
